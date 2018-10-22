05238c33801540e7af2d5402f987f9275cebd161
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2018-10-21 22:01:02 -0500

    refactor module app to use factories

--- app/src/main/java/co/anbora/labs/cleanlogin/Injecction.kt
+++ app/src/main/java/co/anbora/labs/cleanlogin/Injecction.kt
@@ -5,15 +5,19 @@ import co.anbora.labs.cleanlogin.domain.auth.service.Auth
 import co.anbora.labs.cleanlogin.device.auth.service.google.AuthGoogle
 import co.anbora.labs.cleanlogin.domain.auth.controller.AuthController
 import co.anbora.labs.cleanlogin.device.auth.controller.google.GoogleAuthController
+import co.anbora.labs.cleanlogin.device.auth.factory.AuthFactory
+import co.anbora.labs.cleanlogin.device.auth.factory.facebook.AuthFacebookFactory
+import co.anbora.labs.cleanlogin.device.auth.factory.google.AuthGoogleFactory
 import co.anbora.labs.cleanlogin.device.auth.model.ActivityResult
 import co.anbora.labs.cleanlogin.domain.auth.controller.AuthCallback
+import com.facebook.CallbackManager
 import com.google.firebase.auth.FirebaseAuth
 
-fun provideFirebaseAuthController(activityResult: ActivityResult, mAuth: FirebaseAuth, callback: AuthCallback): AuthController {
-    return GoogleAuthController(activityResult, mAuth, callback)
+fun provideGoogleAuthFactory(context: Activity, defaultWebClientId: String, mAuth: FirebaseAuth): AuthFactory {
+    return AuthGoogleFactory(context, defaultWebClientId, mAuth)
 }
 
-fun provideLoginAuthService(context: Activity, defaultWebClientId: String): Auth {
-    return AuthGoogle(context, defaultWebClientId)
+fun provideFacebookAuthFactory(mAuth: FirebaseAuth, callbackManager: CallbackManager, callback: AuthCallback): AuthFactory {
+    return AuthFacebookFactory(mAuth, callbackManager, callback)
 }
 
--- app/src/main/java/co/anbora/labs/cleanlogin/ui/MainActivity.kt
+++ app/src/main/java/co/anbora/labs/cleanlogin/ui/MainActivity.kt
@@ -3,14 +3,14 @@ package co.anbora.labs.cleanlogin.ui
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import co.anbora.labs.cleanlogin.R
+import co.anbora.labs.cleanlogin.*
+import co.anbora.labs.cleanlogin.device.auth.factory.AuthFactory
 import co.anbora.labs.cleanlogin.domain.auth.service.Auth
 import co.anbora.labs.cleanlogin.domain.auth.controller.AuthCallback
 import co.anbora.labs.cleanlogin.domain.auth.controller.AuthController
 import co.anbora.labs.cleanlogin.device.auth.model.ActivityResult
 import co.anbora.labs.cleanlogin.domain.model.User
-import co.anbora.labs.cleanlogin.provideFirebaseAuthController
-import co.anbora.labs.cleanlogin.provideLoginAuthService
+import com.facebook.CallbackManager
 import com.facebook.login.widget.LoginButton
 import com.google.android.gms.common.SignInButton
 import com.google.firebase.auth.FirebaseAuth
@@ -18,42 +18,43 @@ import com.google.firebase.auth.FirebaseAuth
 class MainActivity : AppCompatActivity() {
 
     private lateinit var authService: Auth
-    private lateinit var authController: AuthController
+    private lateinit var authCallback: AuthCallback
+    private lateinit var authFactory: AuthFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        this.authCallback = InfoUserCallback()
+
         val googleSignInButton = findViewById<SignInButton>(R.id.google_sign_in)
         googleSignInButton.setOnClickListener {
-            this.authService = provideLoginAuthService(this, getString(R.string.default_web_client_id))
-            this.authService.loginRequest()
+
+            this.authFactory = provideGoogleAuthFactory(this, getString(R.string.default_web_client_id), FirebaseAuth.getInstance())
+            this.setAuthService(authFactory)
+
         }
 
         val facebookLoginButton = findViewById<LoginButton>(R.id.facebook_sign_in)
         facebookLoginButton.setReadPermissions("email", "public_profile")
         facebookLoginButton.setOnClickListener {
-            //TO DO assign value to authservice
-            this.authService.loginRequest()
+
+            this.authFactory = provideFacebookAuthFactory(FirebaseAuth.getInstance(), CallbackManager.Factory.create(), this.authCallback)
+            this.setAuthService(authFactory)
         }
 
+    }
+
+    private fun setAuthService(factory: AuthFactory) {
+        this.authService = factory.getAuthService()
+        this.authService.loginRequest()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        this.authService.login(provideFirebaseAuthController(ActivityResult(this,
-                                                            requestCode, resultCode, data!!),
-                FirebaseAuth.getInstance(),
-                object : AuthCallback{
-            override fun onSuccess(user: User) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onError() {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        }))
+        this.authService.login(this.authFactory.getAuthController(ActivityResult(this,
+                                                            requestCode, resultCode, data!!), this.authCallback))
 
     }
 }
