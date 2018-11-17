8a439a0ec8b14e7efe9831affcf6ca2e010de428
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2018-11-17 17:48:33 -0500

    refactoring auth method for anonymous

--- device/src/main/java/co/anbora/labs/cleanlogin/device/auth/controller/anonymous/AnonymousAuthController.kt
+++ device/src/main/java/co/anbora/labs/cleanlogin/device/auth/controller/anonymous/AnonymousAuthController.kt
@@ -1,10 +1,20 @@
 package co.anbora.labs.cleanlogin.device.auth.controller.anonymous
 
+import co.anbora.labs.cleanlogin.device.auth.behavior.AnonymousAuthBehavior
 import co.anbora.labs.cleanlogin.domain.auth.controller.AuthController
+import com.google.firebase.auth.FirebaseAuth
 
 class AnonymousAuthController : AuthController {
 
-    override fun login() {
+    private val mAuth: FirebaseAuth
+    private val authBehavior: AnonymousAuthBehavior
 
+    constructor(mAuth: FirebaseAuth, authBehavior: AnonymousAuthBehavior) {
+        this.mAuth = mAuth
+        this.authBehavior = authBehavior
+    }
+
+    override fun login() {
+        this.authBehavior.onLoginComplete()
     }
 }
--- device/src/main/java/co/anbora/labs/cleanlogin/device/auth/service/anonymous/AuthAnonymous.kt
+++ device/src/main/java/co/anbora/labs/cleanlogin/device/auth/service/anonymous/AuthAnonymous.kt
@@ -1,5 +1,9 @@
 package co.anbora.labs.cleanlogin.device.auth.service.anonymous
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import co.anbora.labs.cleanlogin.domain.auth.AuthEnum
 import co.anbora.labs.cleanlogin.domain.auth.controller.AuthCallback
 import co.anbora.labs.cleanlogin.domain.auth.service.Auth
 import co.anbora.labs.cleanlogin.domain.model.User
@@ -7,29 +11,18 @@ import com.google.firebase.auth.FirebaseAuth
 
 class AuthAnonymous : Auth {
 
+    private val context: Activity
     private val mAuth: FirebaseAuth
     private val callback: AuthCallback
 
-    constructor(mAuth: FirebaseAuth, callback: AuthCallback) {
+    constructor(mAuth: FirebaseAuth, callback: AuthCallback, context: Activity) {
         this.mAuth = mAuth
         this.callback = callback
+        this.context = context
     }
 
     override fun loginRequest() {
-        this.mAuth.signInAnonymously().addOnCompleteListener {
-            if (it.isSuccessful) {
-
-                mAuth.currentUser?.let {
-                    val user = User.Builder()
-                            .id(it.uid)
-                            .name(it.displayName)
-                            .build()
-                    callback.onSuccess(user)
-                }
-            } else {
-
-                callback.onError()
-            }
-        }
+        val intentResult = Intent()
+        context.setResult(AuthEnum.ANONYMOUS.authValue, intentResult)
     }
 }
