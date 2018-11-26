24702e1a7e3106dc00e92f5aac306bca5f948e25
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2018-11-25 20:14:33 -0500

    refactor twitter auth

--- device/src/main/java/co/anbora/labs/cleanlogin/device/auth/service/twitter/AuthTwitter.kt
+++ device/src/main/java/co/anbora/labs/cleanlogin/device/auth/service/twitter/AuthTwitter.kt
@@ -1,8 +1,6 @@
 package co.anbora.labs.cleanlogin.device.auth.service.twitter
 
-import co.anbora.labs.cleanlogin.domain.auth.controller.AuthCallback
 import co.anbora.labs.cleanlogin.domain.auth.service.Auth
-import com.google.firebase.auth.FirebaseAuth
 import com.twitter.sdk.android.core.Callback
 import com.twitter.sdk.android.core.TwitterSession
 import com.twitter.sdk.android.core.identity.TwitterLoginButton
@@ -11,16 +9,12 @@ import com.twitter.sdk.android.core.identity.TwitterLoginButton
 class AuthTwitter : Auth {
 
     private val twitterLoginButton: TwitterLoginButton
-    private val callback: AuthCallback
-    private val mAuth: FirebaseAuth
     private val twitterCallback: Callback<TwitterSession>
 
-    constructor(mAuth: FirebaseAuth, twitterLoginButton: TwitterLoginButton,
-                callback: AuthCallback, twitterCallback: Callback<TwitterSession>) {
+    constructor(twitterLoginButton: TwitterLoginButton,
+                twitterCallback: Callback<TwitterSession>) {
 
-        this.mAuth = mAuth
         this.twitterLoginButton = twitterLoginButton
-        this.callback = callback
         this.twitterCallback = twitterCallback
     }
 
