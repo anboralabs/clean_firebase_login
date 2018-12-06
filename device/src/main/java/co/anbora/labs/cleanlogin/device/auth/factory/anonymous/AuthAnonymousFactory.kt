cc901188e1c00e112a981283eb96d03d604f2c22
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2018-12-05 22:03:05 -0500

    refactor factories

--- device/src/main/java/co/anbora/labs/cleanlogin/device/auth/factory/anonymous/AuthAnonymousFactory.kt
+++ device/src/main/java/co/anbora/labs/cleanlogin/device/auth/factory/anonymous/AuthAnonymousFactory.kt
@@ -1,5 +1,7 @@
 package co.anbora.labs.cleanlogin.device.auth.factory.anonymous
 
+import android.app.Activity
+import co.anbora.labs.cleanlogin.device.auth.behavior.AnonymousAuthBehavior
 import co.anbora.labs.cleanlogin.device.auth.controller.anonymous.AnonymousAuthController
 import co.anbora.labs.cleanlogin.device.auth.factory.AuthFactory
 import co.anbora.labs.cleanlogin.device.auth.model.ActivityResult
@@ -11,19 +13,24 @@ import com.google.firebase.auth.FirebaseAuth
 
 class AuthAnonymousFactory : AuthFactory {
 
-    private val mAuth: FirebaseAuth
     private val callback: AuthCallback
+    private val context: Activity
+    private val authBehavior: AnonymousAuthBehavior
 
-    constructor(mAuth: FirebaseAuth, callback: AuthCallback) {
-        this.mAuth = mAuth
+    constructor(context: Activity,
+                callback: AuthCallback,
+                authBehavior: AnonymousAuthBehavior) {
+
         this.callback = callback
+        this.context = context
+        this.authBehavior = authBehavior
     }
 
     override fun getAuthService(): Auth {
-        return AuthAnonymous(mAuth, callback)
+        return AuthAnonymous(callback, context)
     }
 
     override fun getAuthController(activityResult: ActivityResult, callback: AuthCallback): AuthController {
-        return AnonymousAuthController()
+        return AnonymousAuthController(this.authBehavior, activityResult)
     }
 }
--- device/src/main/java/co/anbora/labs/cleanlogin/device/auth/factory/twitter/AuthTwitterFactory.kt
+++ device/src/main/java/co/anbora/labs/cleanlogin/device/auth/factory/twitter/AuthTwitterFactory.kt
@@ -1,5 +1,6 @@
 package co.anbora.labs.cleanlogin.device.auth.factory.twitter
 
+import co.anbora.labs.cleanlogin.device.auth.behavior.AuthBehavior
 import co.anbora.labs.cleanlogin.device.auth.controller.twitter.TwitterAuthController
 import co.anbora.labs.cleanlogin.device.auth.factory.AuthFactory
 import co.anbora.labs.cleanlogin.device.auth.model.ActivityResult
@@ -7,24 +8,30 @@ import co.anbora.labs.cleanlogin.device.auth.service.twitter.AuthTwitter
 import co.anbora.labs.cleanlogin.domain.auth.controller.AuthCallback
 import co.anbora.labs.cleanlogin.domain.auth.controller.AuthController
 import co.anbora.labs.cleanlogin.domain.auth.service.Auth
-import com.google.firebase.auth.FirebaseAuth
+import com.twitter.sdk.android.core.Callback
+import com.twitter.sdk.android.core.TwitterSession
 import com.twitter.sdk.android.core.identity.TwitterLoginButton
 
 class AuthTwitterFactory : AuthFactory {
 
-    private val authService: Auth
     private val twitterLoginButton: TwitterLoginButton
+    private val twitterCallback: Callback<TwitterSession>
+    private val authBehavior: AuthBehavior
 
-    constructor(mAuth: FirebaseAuth, twitterLoginButton: TwitterLoginButton, callback: AuthCallback) {
-        this.authService = AuthTwitter(mAuth, twitterLoginButton, callback)
+    constructor(twitterLoginButton: TwitterLoginButton,
+                twitterCallback: Callback<TwitterSession>,
+                authBehavior: AuthBehavior) {
+
         this.twitterLoginButton = twitterLoginButton
+        this.twitterCallback = twitterCallback
+        this.authBehavior = authBehavior
     }
 
     override fun getAuthService(): Auth {
-        return this.authService
+        return AuthTwitter(twitterLoginButton, this.twitterCallback)
     }
 
     override fun getAuthController(activityResult: ActivityResult, callback: AuthCallback): AuthController {
-        return TwitterAuthController(activityResult, twitterLoginButton)
+        return TwitterAuthController(activityResult, twitterLoginButton, authBehavior)
     }
 }
